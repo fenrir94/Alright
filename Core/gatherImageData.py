7fdfa7fd87840b8190862a60dd8ee6a3e32255cc
RYOO KANG HYUN <ryoogh96@gmail.com>
2019-08-06 22:20:54 +0900

    from gatherImageData import getImageDatas 로 불러와서 getImageDatas(keyword)로 사용 images/background/keyword 장소에 저장

--- Core/gatherImageData.py
+++ Core/gatherImageData.py
@@ -3,9 +3,20 @@ import requests
 import urllib.request
 from bs4 import BeautifulSoup
 
-def getImageDatas(url, max_page, cur_dir):
+def getImageDatas(keyword):
+
+    url = 'https://pixabay.com/ko/images/search/'
+    cur_dir = os.path.abspath("../images/background/" + keyword + "/")
+    main_page_src = requests.get(url + keyword + '/?pagi=1')
+    plain_main_page_text = main_page_src.text
+    soup_main_page = BeautifulSoup(plain_main_page_text, 'lxml')
+    max_page_str = soup_main_page.find('form', {'class': 'add_search_params pure-form hide-xs hide-sm hide-md'})
+    for s in max_page_str.get_text().split():
+        if s.isdigit():
+            max_page = int(s)
+
     for i in range(1, max_page + 1):
-        cur_url = url + '?pagi=' + str(i)
+        cur_url = url + keyword + '/?pagi=' + str(i)
         page_src = requests.get(cur_url)
         plain_page_text = page_src.text
         soup_page = BeautifulSoup(plain_page_text, 'lxml')
@@ -23,17 +34,4 @@ def getImageDatas(url, max_page, cur_dir):
 
 if __name__ == '__main__':
 
-
-    main_page_src = requests.get('https://pixabay.com/ko/images/search/도로/')
-    plain_main_page_text = main_page_src.text
-    soup_main_page = BeautifulSoup(plain_main_page_text, 'lxml')
-    max_page_str = soup_main_page.find('form', {'class': 'add_search_params pure-form hide-xs hide-sm hide-md'})
-    for s in max_page_str.get_text().split():
-                if s.isdigit():
-                    max_page = int(s)
-
-    cur_dir = os.path.abspath("../images/background/도로/")
-
-    getImageDatas('https://pixabay.com/ko/images/search/도로/', max_page, cur_dir)
-
-
+    getImageDatas("도로")
